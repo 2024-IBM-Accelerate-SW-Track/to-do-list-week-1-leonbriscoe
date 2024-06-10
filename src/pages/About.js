5ae19608be66579ad9d5db68178816f546a432e2
leondanquah <leondanq@buffalo.edu>
2024-06-10 16:01:20 -0400

    Attempt 1

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import "./About.css";
+import profilePic from '../assets/profile_pic_name.jpg'; 
+
   
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+       <div>
+<div className="split left">
+<div className="centered">
+<img 
+className="profile_image"
+// Image goes here
+src = {profilePic}
+alt="Profile Pic"
+></img>
+</div>
+</div>
+<div className="split right">
+<div className="centered">
+<div className="name_title">Leon Danquah</div>
+<div className="brief_description">
+I am a rising Junior attending the University at Buffalo majoring in Computer Science.During my free time, I enjoy listening to music, working out, and watching anime.
+</div>
+</div>
+</div>
+</div>
+
       </div>
-    )
+    );
   }
 }
